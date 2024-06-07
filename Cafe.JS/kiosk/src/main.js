fb625f798f7881ac9fb13a7679663323db980118
minju132 <minju132123@gmail.com>
2024-06-07 13:32:15 +0900

    first commit

--- Cafe.JS/kiosk/src/main.js
+++ Cafe.JS/kiosk/src/main.js
@@ -1,9 +1,4 @@
-
-
 import { createApp } from 'vue'
 import App from './App.vue'
-import Video from './components/video/video.vue';
-
-// app.component('Video', Video);
 
 createApp(App).mount('#app')
